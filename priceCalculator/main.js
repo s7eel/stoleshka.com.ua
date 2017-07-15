7888e82ee26b4164770c31f9a75e036f30ee4e97
Hex <Sergeeva.Anastacia@gmail.com>
2017-07-15 21:58:09 +0300

    Add button logic

--- priceCalculator/main.js
+++ priceCalculator/main.js
@@ -1,23 +1,30 @@
 $(function () {
-  var inputData = {
-    // woodBreed: undefined,
-    // bondingType: undefined,
-    // gauge: undefined,
-    // glueType: undefined,
-    // detailsNumber: undefined,
-    // size: undefined,
-    // chamferRemoving: undefined,
-    // complexRadius: undefined,
-    // coveringPreparation: undefined,
-    // covering: undefined,
-    // toningColor: undefined,
-    // discount: false,
-    // packaging: undefined
-  };
+  var inputData = {};
+
+  var $form = $('#calculator');
 
   changeCheckboxBehavior();
 
-  $('#calculator').find('input').on('change', function (event) {
+  $form.on('submit', function (e) {
+    e.preventDefault();
+  });
+
+  $('#refresh, #submit, .close').on('click', function () {
+    $.ajax({
+      url: 'calc.php',
+      type: 'post',
+      dataType: 'application/json',
+      data: $form.serialize(),
+      success: function (data) {
+        resetForm();
+      },
+      error: function () {
+        resetForm();
+      }
+    });
+  });
+
+  $form.find('input').on('change', function (event) {
     var sums;
 
     inputData[event.currentTarget.name] = event.currentTarget.value;
@@ -29,14 +36,19 @@ $(function () {
       }
     }
   });
+
+
+  function resetForm () {
+    $form[0].reset();
+    $form.find('input').trigger('change');
+  }
+
+  function changeCheckboxBehavior () {
+    $("input:checkbox").on('click', function () {
+      var $box = $(this);
+      var group = "input:checkbox[name='" + $box.attr("name") + "']";
+      $(group).prop("checked", false);
+      $box.prop("checked", true);
+    });
+  }
 });
-
-
-function changeCheckboxBehavior () {
-  $("input:checkbox").on('click', function () {
-    var $box = $(this);
-    var group = "input:checkbox[name='" + $box.attr("name") + "']";
-    $(group).prop("checked", false);
-    $box.prop("checked", true);
-  });
-}
