f954e864c23c6c53d32e92a51fec0bff976eb1ee
Marinailch <marina_ilch@mail.ru>
2017-07-16 16:14:46 +0300

    fooer

--- pages/footer.php
+++ pages/footer.php
@@ -1,114 +1,116 @@
 <!--// FOOTER START //-->
-<footer>
-    <div class="widget kode_widget3">
-        <div class="container">
-            <div class="col-md-3 col-sm-6">
-                <div class="kode_widget3_des">
-                    <a href="index.html#"><img src="images/logo9.png" style="max-width: 85%; margin-top: 17px" alt=""></a>
-                    <p>Изделия из натурального дерева под заказ, по индивидуальным размерам. Высокое качество, кратчайшие сроки</p>
-                    <div class="kode_widget3_icon">
-                        <h6>Контакты</h6>
-                        <ul>
-                            <li><a href="index.html#"><i class="fa fa-facebook"></i></a></li>
-                            <li><a href="index.html#"><i class="fa fa-instagram"></i></a></li>
-                            <li><a href="index.html#"><i class="fa fa-whatsapp"></i></a></li>
-                            <li><a href="index.html#"><i class="fa fa-skype"></i></a></li>
-                            <li><a href="index.html#"><i class="fa fa-youtube"></i></a></li>
+            <footer>
+                <div class="widget kode_widget3">
+                    <div class="container">
+                        <div class="col-md-3 col-sm-6">
+                            <div class="kode_widget3_des">
+                                <a href="index.html#"><img src="images/logo9.png" style="max-width: 85%; margin-top: 17px" alt=""></a>
+                                <p>Изделия из натурального дерева под заказ, по индивидуальным размерам. Высокое качество, кратчайшие сроки</p>
+                                <div class="kode_widget3_icon">
+                                    <h6>Контакты</h6>
+                                    <ul>
+                                        <li><a href="index.html#"><i class="fa fa-facebook"></i></a></li>
+                                        <li><a href="index.html#"><i class="fa fa-instagram"></i></a></li>
+                                        <li><a href="index.html#"><i class="fa fa-whatsapp"></i></a></li>
+                                        <li><a href="index.html#"><i class="fa fa-skype"></i></a></li>
+                                        <li><a href="index.html#"><i class="fa fa-youtube"></i></a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-3 col-sm-6">
+                            <div class="kode_widget3_content">
+                                <h4>Продукция</h4>
+                                <ul class="widget3_services">
+                                    <li><a href="index.html#">Столешницы</a></li>
+                                    <li><a href="index.html#">Ступени</a></li>
+                                    <li><a href="index.html#">Подоконники</a></li>
+                                    <li><a href="index.html#">Прямые фасады</a></li>
+                                    <li><a href="index.html#">Мебельный щит</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="col-md-3 col-sm-6">
+                            <div class="kode_widget3_content">
+                                <h4>Контакты</h4>
+                                <div class="kode_widget_contact">
+                                    <ul>
+                                        <li>
+                                            <div class="kode_top_info2 widget">
+                                                <span><i class="fa fa-paper-plane-o"></i></span>
+                                                <div class="kode_top_info_text">
+                                                    <p>г. Днепр, <br>ул. Саранская, 91д</p>
+                                                </div>
+                                            </div>
+                                        </li>
+                                        <li>
+                                            <div class="kode_top_info2 widget">
+                                                <span><i class="fa fa-phone"></i></span>
+                                                <div class="kode_top_info_text">
+                                                    <a href="index.html">+1234 (4563) 7894</a>
+                                                    <a href="index.html">+1234 (4563) 7894</a>
+                                                </div>
+                                            </div>
+                                        </li>
+                                        <li>
+                                            <div class="kode_top_info2 widget">
+                                                <span><i class="fa fa-envelope-o"></i></span>
+                                                <div class="kode_top_info_text">
+                                                    <a href="index.html">stoleshka.com.ua@gmail.com</a>
+                                                    <a href="index.html">stoleshka.com.ua@gmail.com</a>
+                                                </div>
+                                            </div>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-3 col-sm-6">
+                            <div class="kode_widget3_content">
+                                <h4>Часы работы</h4>
+                                <div class="kode_widget_contact twitter">
+                                    <ul>
+                                        <li>
+                                            <div class="kode_top_info2 widget widget2">
+                                                <i class="fa fa-twitter"></i>
+                                                <div class="kode_top_info_text">
+                                                    <p><a href="index.html#"></a>Понедельник - Пятница</p>
+                                                    <span>с 8-00 до 16-00</span>
+                                                </div>
+                                            </div>
+                                        </li>
+                                        <li>
+                                            <div class="kode_top_info2 widget widget2">
+                                                <i class="fa fa-twitter"></i>
+                                                <div class="kode_top_info_text">
+                                                    <p><a href="index.html#"></a>Суббота и Воскресенье</p>
+                                                    <span>Выходной</span>
+                                                </div>
+                                            </div>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="kode_copyright_widget">
+                    <div class="container">
+                        <div class="kode_copyright_text">
+                            <p></p>
+                        </div>
+                        <ul class="kode_copyright_navi">
+                            <li><a href="index.html#">Главная</a></li>
+                            <li><a href="index.html#">О нас</a></li>
+                            <li><a href="index.html#">Продукция</a></li>
+                            <li><a href="index.html#">Цены</a></li>
+                            <li><a href="index.html#">Доставка</a></li>
+                            <li><a href="index.html#">Блог</a></li>
                         </ul>
                     </div>
                 </div>
-            </div>
-            <div class="col-md-3 col-sm-6">
-                <div class="kode_widget3_content">
-                    <h4>Продукция</h4>
-                    <ul class="widget3_services">
-                        <li><a href="index.html#">столешницы;</a></li>
-                        <li><a href="index.html#">ступени</a></li>
-                        <li><a href="index.html#">подоконники;</a></li>
-                        <li><a href="index.html#">прямые фасады</a></li>
-                    </ul>
-                </div>
-            </div>
-            <div class="col-md-3 col-sm-6">
-                <div class="kode_widget3_content">
-                    <h4>Контакты</h4>
-                    <div class="kode_widget_contact">
-                        <ul>
-                            <li>
-                                <div class="kode_top_info2 widget">
-                                    <span><i class="fa fa-paper-plane-o"></i></span>
-                                    <div class="kode_top_info_text">
-                                        <p>г. Днепр, ул. Саранская 91д</p>
-                                    </div>
-                                </div>
-                            </li>
-                            <li>
-                                <div class="kode_top_info2 widget">
-                                    <span><i class="fa fa-phone"></i></span>
-                                    <div class="kode_top_info_text">
-                                        <a href="index.html">+1234 (4563) 7894</a>
-                                        <a href="index.html">+1234 (4563) 7894</a>
-                                    </div>
-                                </div>
-                            </li>
-                            <li>
-                                <div class="kode_top_info2 widget">
-                                    <span><i class="fa fa-envelope-o"></i></span>
-                                    <div class="kode_top_info_text">
-                                        <a href="index.html">youremail@gmail.com</a>
-                                        <a href="index.html">trusttransport_net</a>
-                                    </div>
-                                </div>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-3 col-sm-6">
-                <div class="kode_widget3_content">
-                    <h4>twitter feeds</h4>
-                    <div class="kode_widget_contact twitter">
-                        <ul>
-                            <li>
-                                <div class="kode_top_info2 widget widget2">
-                                    <i class="fa fa-twitter"></i>
-                                    <div class="kode_top_info_text">
-                                        <p><a href="index.html#"></a>transport completely synergize theme is good</p>
-                                        <span>3 minutes ago</span>
-                                    </div>
-                                </div>
-                            </li>
-                            <li>
-                                <div class="kode_top_info2 widget widget2">
-                                    <i class="fa fa-twitter"></i>
-                                    <div class="kode_top_info_text">
-                                        <p><a href="index.html#"></a>transport completely synergize theme is good</p>
-                                        <span>3 minutes ago</span>
-                                    </div>
-                                </div>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
+            </footer>   
         </div>
-    </div>
-    <div class="kode_copyright_widget">
-        <div class="container">
-            <div class="kode_copyright_text">
-                <p></p>
-            </div>
-            <ul class="kode_copyright_navi">
-                <li><a href="index.html#">Home</a></li>
-                <li><a href="index.html#">About us</a></li>
-                <li><a href="index.html#">Services</a></li>
-                <li><a href="index.html#">Track & Trace</a></li>
-                <li><a href="index.html#">News</a></li>
-                <li><a href="index.html#">Contact us</a></li>
-            </ul>
-        </div>
-    </div>
-</footer>
 </div>
 <!--kode_wraper end-->
 <script src="js/jquery-3.1.1-jquery.min.js"></script>
