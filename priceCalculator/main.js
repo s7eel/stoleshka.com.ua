3329a8725b8bfc6d88b0df840f57fc30f0798eaf
s7eel <s7eell@gmail.com>
2017-07-17 22:42:59 +0300

    PSR-4

--- priceCalculator/main.js
+++ priceCalculator/main.js
@@ -1,58 +1,59 @@
 $(function () {
-  var inputData = {};
+    var inputData = {};
 
-  var $form = $('#calculator');
+    var $form = $('#calculator');
 
-  changeCheckboxBehavior();
+    changeCheckboxBehavior();
 
-  $form.on('submit', function (e) {
-    e.preventDefault();
-  });
-
-  $('#refresh, #submit, .close').on('click', function () {
-    var data = {};
-    $form.serializeArray().map(function (item) {
-      return data[item.name] = item.value;
+    $form.on('submit', function (e) {
+        e.preventDefault();
     });
-    data.totalWithDiscount = $('#totalWithDiscount').html();
 
-    $.ajax({
-      url: '../index.php?page=errorPage',
-      type: 'post',
-      dataType: 'json',
-      data: JSON.stringify(data),
-      success: function (data) {
-        resetForm();
-      },
-      error: function (e) {}
+    $('#refresh, #submit, .close').on('click', function () {
+        var data = {};
+        $form.serializeArray().map(function (item) {
+            return data[item.name] = item.value;
+        });
+        data.totalWithDiscount = $('#totalWithDiscount').html();
+
+        $.ajax({
+            url: '../index.php?page=errorPage',
+            type: 'post',
+            dataType: 'json',
+            data: JSON.stringify(data),
+            success: function (data) {
+                resetForm();
+            },
+            error: function (e) {
+            }
+        });
     });
-  });
 
-  $form.find('input').on('change', function (event) {
-    var sums;
+    $form.find('input').on('change', function (event) {
+        var sums;
 
-    inputData[event.currentTarget.name] = event.currentTarget.value;
-    sums = calcSums(inputData);
+        inputData[event.currentTarget.name] = event.currentTarget.value;
+        sums = calcSums(inputData);
 
-    for (var key in sums) {
-      if (sums.hasOwnProperty(key)) {
-        $('#' + key).html(Number(sums[key] || 0).toFixed());
-      }
+        for (var key in sums) {
+            if (sums.hasOwnProperty(key)) {
+                $('#' + key).html(Number(sums[key] || 0).toFixed());
+            }
+        }
+    });
+
+
+    function resetForm() {
+        $form[0].reset();
+        $form.find('input').trigger('change');
     }
-  });
 
-
-  function resetForm () {
-    $form[0].reset();
-    $form.find('input').trigger('change');
-  }
-
-  function changeCheckboxBehavior () {
-    $("input:checkbox").on('click', function () {
-      var $box = $(this);
-      var group = "input:checkbox[name='" + $box.attr("name") + "']";
-      $(group).prop("checked", false);
-      $box.prop("checked", true);
-    });
-  }
+    function changeCheckboxBehavior() {
+        $("input:checkbox").on('click', function () {
+            var $box = $(this);
+            var group = "input:checkbox[name='" + $box.attr("name") + "']";
+            $(group).prop("checked", false);
+            $box.prop("checked", true);
+        });
+    }
 });
