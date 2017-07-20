41b8a833c5fc626c1ab8ef4c15b77cf160aee4d8
s7eel <s7eell@gmail.com>
2017-07-20 15:35:31 +0300

    added coeffic page

--- admin/pages/coeffic.php
+++ admin/pages/coeffic.php
@@ -1,8 +1,19 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: s7eel
- * Date: 20.07.17
- * Time: 14:41
- */
-var_dump($arr);
+<form method="POST">
+        Коэффициент 'Снятие фаски по периметру'<input type="text" name="chamferRemovingPrice" placeholder="<?=$arr[0]['chamferRemovingPrice']?>"><br>
+        Криволинейное форматирование<input type="text" name="complexRadiusPrice" placeholder="<?=$arr[0]['complexRadiusPrice']?>"><br>
+        Подготовка к покрытию<input type="text" name="coveringPreparationPrice" placeholder="<?=$arr[0]['coveringPreparationPrice']?>"><br>
+        Клей влагостойкий<input type="text" name="waterproof" placeholder="<?=$arr[0]['waterproof']?>"><br>
+        Клей невлагостойкий<input type="text" name="notWaterproof" placeholder="<?=$arr[0]['notWaterproof']?>"><br>
+        Покрытие: Лак<input type="text" name="polish" placeholder="<?=$arr[0]['polish']?>"><br>
+        Покрытие: Лак+Тон<input type="text" name="polishWithColor" placeholder="<?=$arr[0]['polishWithColor']?>"><br>
+        Покрытие: Без Покрытия<input type="text" name="noCovering" placeholder="<?=$arr[0]['noCovering']?>"><br>
+        Упаковка<input type="text" name="packagingPrice" placeholder="<?=$arr[0]['packagingPrice']?>"><br>
+        Ясень<input type="text" name="ash" placeholder="<?=$arr[0]['ash']?>"><br>
+        Дуб<input type="text" name="oak" placeholder="<?=$arr[0]['oak']?>"><br>
+        Клееный массив<input type="text" name="glued" placeholder="<?=$arr[0]['glued']?>"><br>
+        Срощенный массив<input type="text" name="spliced" placeholder="<?=$arr[0]['spliced']?>"><br>
+        Толщина 20мм<input type="text" name="gauge20" placeholder="<?=$arr[0]['gauge20']?>"><br>
+        Толщина 30мм<input type="text" name="gauge30" placeholder="<?=$arr[0]['gauge30']?>"><br>
+        Толщина 40мм<input type="text" name="gauge40" placeholder="<?=$arr[0]['gauge40']?>"><br>
+        Скидка<input type="text" name="discount" placeholder="<?=$arr[0]['discount']?>"><br>
+</form>
