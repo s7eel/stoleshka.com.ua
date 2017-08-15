539400c9a16bb3752bed1b2f5fed37f51ed450ed
Marinailch <marina_ilch@mail.ru>
2017-08-15 23:40:56 +0300

    seo meta+google

--- pages/parts/header.php
+++ pages/parts/header.php
@@ -5,6 +5,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?=$title?></title>
+    <meta name="keywords"  content="ступени, столешницы, подоконники, прямые фасады, мебельный щит, изделия из натурального дерева, дуб, ясень">
+    <meta name="description" content="Производим изделия из натурального дерева (дуб, ясень) - ступени, столешницы, подоконники, прямые фасады, мебельный щит. Кратчайшие сроки. Цены от производителя!">
     <!--css/bootstrap.min.css start-->
     <link href="/css/bootstrap.css" rel="stylesheet">
     <link rel="shortcut icon" href="/images/favicon.ico" type="image/x-icon">
@@ -73,6 +75,17 @@
                 discount: <?=$arr[0]['discount']?>
             };</script>
     <?}?>
+
+    <script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-104664076-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
 </head>
 <body class="light">
 <!--// KODE WRAPER START //-->
